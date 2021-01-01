db246cceb55c62c84246265d6c24fb9d91b9fe97
amanc1248 <amanc1248@gmail.com>
2021-01-01 17:18:27 +0545

    added tailor changepassword

---
+++ src/Components/Tailor/TailorStore/TailorAccountDetails/AccountInformation/TailorAccountInformation.js
@@ -0,0 +1,156 @@
+import React from "react";
+import "./TailorAccountInformation.css";
+function TailorAccountInformation() {
+  return (
+    <div className="tailor__account__information">
+      <div className="tailor__account__information">
+        Please update your personal account details, update your address book or
+        change your email setting here.
+      </div>
+      <div className="tailor__account__information__container">
+        <div className="tailor__details">
+          <span>
+            {" "}
+            <strong>First Name:</strong>{" "}
+          </span>
+          <span>Aman</span>
+        </div>
+        <div className="tailor__details">
+          <span>
+            {" "}
+            <strong>Last Name:</strong>{" "}
+          </span>
+          <span>Chaudhary</span>
+        </div>
+        <div className="tailor__details">
+          <span>
+            {" "}
+            <strong>Location:</strong>{" "}
+          </span>
+          <span>Itahari-07, Sunsari</span>
+        </div>
+        <div className="tailor__details">
+          <span>
+            {" "}
+            <strong>Phone:</strong>{" "}
+          </span>
+          <span>+9779804355969</span>
+        </div>
+        <div className="tailor__details">
+          <span>
+            {" "}
+            <strong>Email:</strong>{" "}
+          </span>
+          <span>amanc1248@gmail.com</span>
+        </div>
+      </div>
+      <div className="tailor__account__information__buttons row ">
+        <div className="tailor__edit__button__container col-6">
+          <button
+            className="tailor__edit__button"
+            // onClick={() => dispatch(goToAccountEdit())}
+          >
+            EDIT
+          </button>
+        </div>
+        <div className="tailor__changepassword__button__container col-6">
+          <button
+            className="tailor__changepassword__button"
+            // onClick={() => dispatch(goToChangePassword())}
+          >
+            CHANGE PASSWORD
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export function TailorAccountInformationEdit() {
+  return (
+    <div className="tailor__account__information__edit">
+      <div className="tailor__account__information__edit__instruction">
+        Please update your personal account details, update your address book or
+        change your email setting here.
+      </div>
+      <form action="">
+        <div className="tailor__address__edit__inputs">
+          <label>
+            First Name
+            <input type="text" />
+          </label>
+          <label>
+            Last Name
+            <input type="text" />
+          </label>
+          <label>
+            Location
+            <input type="text" />
+          </label>
+          <label>
+            Phone
+            <input type="text" />
+          </label>
+          <label>
+            Email
+            <input type="text" />
+          </label>
+        </div>
+        <div className="tailor__account__information__buttons row justify-content-between">
+          <div className="tailor__save__button__container col-6">
+            <button className="tailor__save__button">SAVE</button>
+          </div>
+          <div className="tailor__accountCancel__button__container col-6">
+            <button className="tailor__accountCancel__button">CANCEL</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export function TailorAccountInformationChangePassword() {
+  return (
+    <div className="tailor__account__information__change__password">
+      <div className="tailor__account__information__change__password__instruction">
+        Please update your personal account details, update your address book or
+        change your email setting here.
+      </div>
+      <div className="tailor__account__information__change__password__title">
+        <h4>CHANGE PASSWORD</h4>
+      </div>
+      <form action="">
+        <div className="tailor__account__information__change__password__inputs">
+          <label>
+            Old Password
+            <input type="text" />
+          </label>
+          <label>
+            New Password
+            <input type="text" />
+          </label>
+        </div>
+        <div className="tailor__account__information__buttons row justify-content-between">
+          <div className="tailor__change__password__edit__button__container col-6">
+            <button
+              className="tailor__change__password__save__button"
+              //   onClick={() => dispatch(goToChangePassword())}
+            >
+              SAVE
+            </button>
+          </div>
+          <div className="tailor__change__password__button__container col-6">
+            <button
+              className="tailor__cancel__button"
+              //   onClick={() => dispatch(goToChangePassword())}
+            >
+              CANCEL
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default TailorAccountInformation;
--- src/Components/Tailor/TailorStore/TailorAccountDetails/TailorAccountDetails.js
+++ src/Components/Tailor/TailorStore/TailorAccountDetails/TailorAccountDetails.js
@@ -1,11 +1,15 @@
 import React from "react";
+import TailorAccountInformation, { TailorAccountInformationChangePassword, TailorAccountInformationEdit } from "./AccountInformation/TailorAccountInformation";
 import TailorAccountDetailsTabs from "./Tabs/TailorAccountDetailsTabs";
 
 function TailorAccountDetails() {
   return (
     <div className="tailor__account__details">
       <TailorAccountDetailsTabs>
-        <div label="Account Information">This is the ACCOUNT INFO</div>
+        <div label="Account Information">
+          {" "}
+          <TailorAccountInformationChangePassword></TailorAccountInformationChangePassword>{" "}
+        </div>
         <div label="Store Information">This is STORE INFO</div>
         <div label="Payment Options">
           <h1>THis is the PAYMENT INFO</h1>
