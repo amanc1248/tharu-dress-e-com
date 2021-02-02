9caf6788c70f5cf07bbf384ba5f74d39b6102f87
amanc1248 <amanc1248@gmail.com>
2021-02-02 22:54:49 +0545

    ADD: Faqs data

---
+++ src/Data/FooterData/FooterDataFaqs.js
@@ -0,0 +1,166 @@
+const ordersAndPaymentData = [
+  {
+    question:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dapibus maximus erat?",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dapibus maximus erat, ac consequat erat tempus quis. Suspendisse et erat eu sem convallis lobortis eget at ligula. Curabitur non congue urna. Vestibulum eu molestie libero. Donec suscipit massa id luctus finibus. Etiam nec ligula at tellus sollicitudin consectetur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean ultricies convallis nunc, id tincidunt orci feugiat id. Donec dapibus est vitae mi aliquam auc",
+  },
+
+  {
+    question:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dapibus maximus erat?",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dapibus maximus erat, ac consequat erat tempus quis. Suspendisse et erat eu sem convallis lobortis eget at ligula. Curabitur non congue urna. Vestibulum eu molestie libero. Donec suscipit massa id luctus finibus. Etiam nec ligula at tellus sollicitudin consectetur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean ultricies convallis nunc, id tincidunt orci feugiat id. Donec dapibus est vitae mi aliquam auc",
+  },
+  {
+    question:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dapibus maximus erat?",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dapibus maximus erat, ac consequat erat tempus quis. Suspendisse et erat eu sem convallis lobortis eget at ligula. Curabitur non congue urna. Vestibulum eu molestie libero. Donec suscipit massa id luctus finibus. Etiam nec ligula at tellus sollicitudin consectetur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean ultricies convallis nunc, id tincidunt orci feugiat id. Donec dapibus est vitae mi aliquam auc",
+  },
+  {
+    question:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dapibus maximus erat?",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dapibus maximus erat, ac consequat erat tempus quis. Suspendisse et erat eu sem convallis lobortis eget at ligula. Curabitur non congue urna. Vestibulum eu molestie libero. Donec suscipit massa id luctus finibus. Etiam nec ligula at tellus sollicitudin consectetur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean ultricies convallis nunc, id tincidunt orci feugiat id. Donec dapibus est vitae mi aliquam auc",
+  },
+  {
+    question:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dapibus maximus erat?",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dapibus maximus erat, ac consequat erat tempus quis. Suspendisse et erat eu sem convallis lobortis eget at ligula. Curabitur non congue urna. Vestibulum eu molestie libero. Donec suscipit massa id luctus finibus. Etiam nec ligula at tellus sollicitudin consectetur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean ultricies convallis nunc, id tincidunt orci feugiat id. Donec dapibus est vitae mi aliquam auc",
+  },
+];
+const shippingAndDelivery = [
+  {
+    question:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dapibus maximus erat?",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dapibus maximus erat, ac consequat erat tempus quis. Suspendisse et erat eu sem convallis lobortis eget at ligula. Curabitur non congue urna. Vestibulum eu molestie libero. Donec suscipit massa id luctus finibus. Etiam nec ligula at tellus sollicitudin consectetur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean ultricies convallis nunc, id tincidunt orci feugiat id. Donec dapibus est vitae mi aliquam auc",
+  },
+  {
+    question:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dapibus maximus erat?",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dapibus maximus erat, ac consequat erat tempus quis. Suspendisse et erat eu sem convallis lobortis eget at ligula. Curabitur non congue urna. Vestibulum eu molestie libero. Donec suscipit massa id luctus finibus. Etiam nec ligula at tellus sollicitudin consectetur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean ultricies convallis nunc, id tincidunt orci feugiat id. Donec dapibus est vitae mi aliquam auc",
+  },
+  {
+    question:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dapibus maximus erat?",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dapibus maximus erat, ac consequat erat tempus quis. Suspendisse et erat eu sem convallis lobortis eget at ligula. Curabitur non congue urna. Vestibulum eu molestie libero. Donec suscipit massa id luctus finibus. Etiam nec ligula at tellus sollicitudin consectetur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean ultricies convallis nunc, id tincidunt orci feugiat id. Donec dapibus est vitae mi aliquam auc",
+  },
+  {
+    question:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dapibus maximus erat?",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dapibus maximus erat, ac consequat erat tempus quis. Suspendisse et erat eu sem convallis lobortis eget at ligula. Curabitur non congue urna. Vestibulum eu molestie libero. Donec suscipit massa id luctus finibus. Etiam nec ligula at tellus sollicitudin consectetur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean ultricies convallis nunc, id tincidunt orci feugiat id. Donec dapibus est vitae mi aliquam auc",
+  },
+];
+const returnsAndExchange = [
+  {
+    question:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dapibus maximus erat?",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dapibus maximus erat, ac consequat erat tempus quis. Suspendisse et erat eu sem convallis lobortis eget at ligula. Curabitur non congue urna. Vestibulum eu molestie libero. Donec suscipit massa id luctus finibus. Etiam nec ligula at tellus sollicitudin consectetur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean ultricies convallis nunc, id tincidunt orci feugiat id. Donec dapibus est vitae mi aliquam auc",
+  },
+
+  {
+    question:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dapibus maximus erat?",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dapibus maximus erat, ac consequat erat tempus quis. Suspendisse et erat eu sem convallis lobortis eget at ligula. Curabitur non congue urna. Vestibulum eu molestie libero. Donec suscipit massa id luctus finibus. Etiam nec ligula at tellus sollicitudin consectetur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean ultricies convallis nunc, id tincidunt orci feugiat id. Donec dapibus est vitae mi aliquam auc",
+  },
+  {
+    question:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dapibus maximus erat?",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dapibus maximus erat, ac consequat erat tempus quis. Suspendisse et erat eu sem convallis lobortis eget at ligula. Curabitur non congue urna. Vestibulum eu molestie libero. Donec suscipit massa id luctus finibus. Etiam nec ligula at tellus sollicitudin consectetur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean ultricies convallis nunc, id tincidunt orci feugiat id. Donec dapibus est vitae mi aliquam auc",
+  },
+  {
+    question:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dapibus maximus erat?",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dapibus maximus erat, ac consequat erat tempus quis. Suspendisse et erat eu sem convallis lobortis eget at ligula. Curabitur non congue urna. Vestibulum eu molestie libero. Donec suscipit massa id luctus finibus. Etiam nec ligula at tellus sollicitudin consectetur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean ultricies convallis nunc, id tincidunt orci feugiat id. Donec dapibus est vitae mi aliquam auc",
+  },
+  {
+    question:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dapibus maximus erat?",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dapibus maximus erat, ac consequat erat tempus quis. Suspendisse et erat eu sem convallis lobortis eget at ligula. Curabitur non congue urna. Vestibulum eu molestie libero. Donec suscipit massa id luctus finibus. Etiam nec ligula at tellus sollicitudin consectetur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean ultricies convallis nunc, id tincidunt orci feugiat id. Donec dapibus est vitae mi aliquam auc",
+  },
+];
+const gifts = [
+  {
+    question:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dapibus maximus erat?",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dapibus maximus erat, ac consequat erat tempus quis. Suspendisse et erat eu sem convallis lobortis eget at ligula. Curabitur non congue urna. Vestibulum eu molestie libero. Donec suscipit massa id luctus finibus. Etiam nec ligula at tellus sollicitudin consectetur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean ultricies convallis nunc, id tincidunt orci feugiat id. Donec dapibus est vitae mi aliquam auc",
+  },
+
+  {
+    question:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dapibus maximus erat?",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dapibus maximus erat, ac consequat erat tempus quis. Suspendisse et erat eu sem convallis lobortis eget at ligula. Curabitur non congue urna. Vestibulum eu molestie libero. Donec suscipit massa id luctus finibus. Etiam nec ligula at tellus sollicitudin consectetur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean ultricies convallis nunc, id tincidunt orci feugiat id. Donec dapibus est vitae mi aliquam auc",
+  },
+  {
+    question:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dapibus maximus erat?",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dapibus maximus erat, ac consequat erat tempus quis. Suspendisse et erat eu sem convallis lobortis eget at ligula. Curabitur non congue urna. Vestibulum eu molestie libero. Donec suscipit massa id luctus finibus. Etiam nec ligula at tellus sollicitudin consectetur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean ultricies convallis nunc, id tincidunt orci feugiat id. Donec dapibus est vitae mi aliquam auc",
+  },
+  {
+    question:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dapibus maximus erat?",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dapibus maximus erat, ac consequat erat tempus quis. Suspendisse et erat eu sem convallis lobortis eget at ligula. Curabitur non congue urna. Vestibulum eu molestie libero. Donec suscipit massa id luctus finibus. Etiam nec ligula at tellus sollicitudin consectetur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean ultricies convallis nunc, id tincidunt orci feugiat id. Donec dapibus est vitae mi aliquam auc",
+  },
+  {
+    question:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dapibus maximus erat?",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dapibus maximus erat, ac consequat erat tempus quis. Suspendisse et erat eu sem convallis lobortis eget at ligula. Curabitur non congue urna. Vestibulum eu molestie libero. Donec suscipit massa id luctus finibus. Etiam nec ligula at tellus sollicitudin consectetur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean ultricies convallis nunc, id tincidunt orci feugiat id. Donec dapibus est vitae mi aliquam auc",
+  },
+];
+const myDasaAccount = [
+  {
+    question:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dapibus maximus erat?",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dapibus maximus erat, ac consequat erat tempus quis. Suspendisse et erat eu sem convallis lobortis eget at ligula. Curabitur non congue urna. Vestibulum eu molestie libero. Donec suscipit massa id luctus finibus. Etiam nec ligula at tellus sollicitudin consectetur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean ultricies convallis nunc, id tincidunt orci feugiat id. Donec dapibus est vitae mi aliquam auc",
+  },
+
+  {
+    question:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dapibus maximus erat?",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dapibus maximus erat, ac consequat erat tempus quis. Suspendisse et erat eu sem convallis lobortis eget at ligula. Curabitur non congue urna. Vestibulum eu molestie libero. Donec suscipit massa id luctus finibus. Etiam nec ligula at tellus sollicitudin consectetur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean ultricies convallis nunc, id tincidunt orci feugiat id. Donec dapibus est vitae mi aliquam auc",
+  },
+  {
+    question:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dapibus maximus erat?",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dapibus maximus erat, ac consequat erat tempus quis. Suspendisse et erat eu sem convallis lobortis eget at ligula. Curabitur non congue urna. Vestibulum eu molestie libero. Donec suscipit massa id luctus finibus. Etiam nec ligula at tellus sollicitudin consectetur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean ultricies convallis nunc, id tincidunt orci feugiat id. Donec dapibus est vitae mi aliquam auc",
+  },
+  {
+    question:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dapibus maximus erat?",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dapibus maximus erat, ac consequat erat tempus quis. Suspendisse et erat eu sem convallis lobortis eget at ligula. Curabitur non congue urna. Vestibulum eu molestie libero. Donec suscipit massa id luctus finibus. Etiam nec ligula at tellus sollicitudin consectetur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean ultricies convallis nunc, id tincidunt orci feugiat id. Donec dapibus est vitae mi aliquam auc",
+  },
+  {
+    question:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dapibus maximus erat?",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam dapibus maximus erat, ac consequat erat tempus quis. Suspendisse et erat eu sem convallis lobortis eget at ligula. Curabitur non congue urna. Vestibulum eu molestie libero. Donec suscipit massa id luctus finibus. Etiam nec ligula at tellus sollicitudin consectetur. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Aenean ultricies convallis nunc, id tincidunt orci feugiat id. Donec dapibus est vitae mi aliquam auc",
+  },
+];
+
+export {
+  ordersAndPaymentData,
+  shippingAndDelivery,
+  returnsAndExchange,
+  gifts,
+  myDasaAccount,
+};
--- src/Footer/FooterFAQs/FooterFAQs.js
+++ src/Footer/FooterFAQs/FooterFAQs.js
@@ -1,7 +1,14 @@
 import React from "react";
 import "./FooterFAQ.css";
 import { FooterFAQsTabs } from "./FooterFAQsTabs";
-function FooterFAQs() {
+import {
+  ordersAndPaymentData,
+  shippingAndDelivery,
+  gifts,
+  returnsAndExchange,
+  myDasaAccount,
+} from "../../Data/FooterData/FooterDataFaqs";
+function FooterFAQs({ faqsData }) {
   return (
     <div className="footer__faqs">
       {/* <img
@@ -19,11 +26,33 @@ function FooterFAQs() {
       </div>
       <div>
         <FooterFAQsTabs>
-          <div label="ORDERS & PAYMENTS"></div>
-          <div label="SHIPPING & DELIVERY"></div>
-          <div label="RETURNS & EXCHANGE"></div>
-          <div label="GIFTS"></div>
-          <div label="MY DASA ACCOUNT"></div>
+          <div label="ORDERS & PAYMENTS">
+            <FAQContent
+              title={"ORDERS & PAYMENTS"}
+              thedata={ordersAndPaymentData}
+            ></FAQContent>
+          </div>
+          <div label="SHIPPING & DELIVERY">
+            <FAQContent
+              title={"SHIPPING & DELIVERY"}
+              thedata={shippingAndDelivery}
+            ></FAQContent>
+          </div>
+          <div label="RETURNS & EXCHANGE">
+            <FAQContent
+              title={"RETURNS & EXCHANGE"}
+              thedata={returnsAndExchange}
+            ></FAQContent>
+          </div>
+          <div label="GIFTS">
+            <FAQContent title={"GIFTS"} thedata={gifts}></FAQContent>
+          </div>
+          <div label="MY DASA ACCOUNT">
+            <FAQContent
+              title={"MY DASA ACCOUNT"}
+              thedata={myDasaAccount}
+            ></FAQContent>
+          </div>
           <div label="CONTACT US"></div>
         </FooterFAQsTabs>
       </div>
@@ -32,3 +61,27 @@ function FooterFAQs() {
 }
 
 export default FooterFAQs;
+function FAQContent({ title, thedata }) {
+  const questionsAnswers = thedata.map((obj) => {
+    return (
+      <div className="question__answer">
+        <div className="question__expand">
+          <div className="question">{obj.question}</div>
+          <div className="question__answer__toogle">+</div>
+        </div>
+        <div className="answer">{obj.answer}</div>
+      </div>
+    );
+  });
+  return (
+    <div className="faq__content">
+      {/* Title Part👇 */}
+      <div className="faq__content__title__expand">
+        <div className="faq__content__title">{title}</div>
+        <div className="faq__content__toogle">Expand All</div>
+      </div>
+      {/* Content part 👇 */}
+      {questionsAnswers}
+    </div>
+  );
+}
