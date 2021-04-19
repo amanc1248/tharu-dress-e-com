193642657b810565921203ddbdb8380b6d8748fc
amanc1248 <amanc1248@gmail.com>
2021-04-19 15:49:10 +0545

    ADD: Place order added

--- frontend/src/Screens/customer/CustomerCheckOutScreen.js
+++ frontend/src/Screens/customer/CustomerCheckOutScreen.js
@@ -1,53 +1,53 @@
-import { Checkbox } from "@material-ui/core";
+import { Button, Checkbox } from "@material-ui/core";
 import React, { useState } from "react";
 import "../../styles/customer/CustomerCheckOutScreen.css";
 import Footer from "../../Components/customer/Footer/Footer";
 import Header from "../../Components/customer/Header";
 import ImageWithTitle from "../../Components/ImageWithTitle";
-import { saveShippingAddress } from "../../actions/cartActions";
+import {
+  savePaymentMethod,
+  saveShippingAddress,
+} from "../../actions/cartActions";
 import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { Col } from "react-bootstrap";
+import { Row, ListGroup, Image, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Message from "../../Components/Message";
+
 function CustomerCheckOutScreen({ history, location }) {
   const [value, setValue] = useState(0);
+  const userLogin = useSelector((state) => state.userLogin);
+  const { userInfo } = userLogin;
+
   const changeValue = () => {
     setValue(1);
+  };
+  const goToPlaceOrder = () => {
+    setValue(2);
   };
   const theLabelData = [
     { label: 1, text: "shipping" },
     { label: 2, text: "payment" },
     { label: 3, text: "placeorder" },
   ];
-  let checkOutFirstLabel = (
-    <div className="the__label">
-      <div className="the__number__label">1</div>
-      <div className="the__text__label">Shipping</div>
-    </div>
-  );
-  let checkOutSecondLabel = (
-    <div className="the__label">
-      <div className="the__number__label">2</div>
-      <div className="the__text__label">Payment</div>
-    </div>
-  );
-  let checkOutThirdLabel = (
-    <div className="the__label">
-      <div className="the__number__label">3</div>
-      <div className="the__text__label">Place Order</div>
-    </div>
-  );
+
   const ourCheckOutTabs = [
     {
-      label: checkOutFirstLabel,
       theTab: <CCShipping value={changeValue}></CCShipping>,
     },
     {
-      label: checkOutSecondLabel,
-      theTab: <CCPayment></CCPayment>,
+      theTab: <CCPayment history={history} value={goToPlaceOrder}></CCPayment>,
     },
     {
-      label: checkOutThirdLabel,
-      theTab: <CCShipping></CCShipping>,
+      theTab: <PlaceOrder history={history}></PlaceOrder>,
     },
   ];
+  useEffect(() => {
+    if (!userInfo) {
+      history.push("/signin");
+    }
+  }, [userLogin, history, userInfo]);
   return (
     <>
       <Header></Header>
@@ -135,15 +135,188 @@ export function CheckOutNextButton() {
   );
 }
 
-export function CCPayment() {
+export function CCPayment({ history, value }) {
+  const cart = useSelector((state) => state.cart);
+  const { shippingAddress } = cart;
+  if (!shippingAddress) {
+    history.push("/checkout");
+  }
+  const [paymentMethod, setPaymentMethod] = useState("PayPal");
+
+  const dispatch = useDispatch();
+  const submitHandler = (e) => {
+    e.preventDefault();
+    dispatch(savePaymentMethod(paymentMethod));
+    value();
+  };
+
   return (
     <div className="cc__payment">
       <div className="payment__method ">
         <h3 className="checkOut__heading"> PAYMENT METHOD</h3>
-        <div className="particular__payment__method">Credit/Debit Card</div>
-        <div className="particular__payment__method">e-sewa mobile wallet</div>
-        <div className="particular__payment__method">Cash On Delivery</div>
+        <div className="the__payment_form">
+          <div>
+            <input
+              className="paypal__input"
+              type="radio"
+              text="Paypal or credit card"
+              id="PayPal"
+              name="paymentMethod"
+              value="PayPal"
+              checked
+              onChange={(e) => setPaymentMethod(e.target.value)}
+            ></input>
+            <label for="payment method">Paypal or credit card</label>
+          </div>
+
+          <div className="checkout__next__button__container ">
+            <button className="checkout__next__button" onClick={submitHandler}>
+              CONTINUE
+            </button>
+          </div>
+        </div>
       </div>
     </div>
+  );
+}
+
+export function PlaceOrder({ history }) {
+  const dispatch = useDispatch();
+  const cart = useSelector((state) => state.cart);
+
+  //   Calculate Prices
+  const addDecimals = (num) => {
+    return (Math.round(num * 100) / 100).toFixed(2);
+  };
+  cart.itemsPrice = addDecimals(
+    cart.cartItems.reduce((acc, item) => acc + item.price * item.qty, 0)
+  );
+  cart.shippingPrice = addDecimals(cart.itemsPrice > 100 ? 0 : 100);
+  cart.taxPrice = addDecimals(Number((0.15 * cart.itemsPrice).toFixed(2)));
+  cart.totalPrice = (
+    Number(cart.itemsPrice) +
+    Number(cart.shippingPrice) +
+    Number(cart.taxPrice)
+  ).toFixed(2);
+  // const orderCreate = useSelector((state) => state.orderCreate);
+  // const { order, success, error } = orderCreate;
+  // useEffect(() => {
+  //   if (success) {
+  //     history.push(`order/${order._id}`);
+  //   }
+  //   // eslint-disable-next-line
+  // }, [success, order]);
+  const placeOrderHandler = () => {
+    // dispatch(
+    //   createOrder({
+    //     orderItems: cart.cartItems,
+    //     shippingAddress: cart.shippingAddress,
+    //     paymentMethod: cart.paymentMethod,
+    //     itemsPrice: cart.itemsPrice,
+    //     taxPrice: cart.taxPrice,
+    //     totalPrice: cart.totalPrice,
+    //   })
+    // );
+  };
+  return (
+    <>
+      <Row className="no-gutters px-2 py-4 text-left">
+        <Col md={8} className="px-2">
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h3 className="placeOrder__heading">SHIPPING</h3>
+
+              <p>
+                <strong>Address: </strong>
+                {cart.shippingAddress.city},{cart.shippingAddress.street},
+              </p>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <h3 className="placeOrder__heading">PAYMENT METHOD</h3>
+              <strong>Method: </strong>
+              {cart.paymentMethod}
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <h3 className="placeOrder__heading">ORDER ITEMS</h3>
+              {cart.cartItems.length === 0 ? (
+                <Message>Your Cart is empty</Message>
+              ) : (
+                <ListGroup variant="flush">
+                  {cart.cartItems.map((item, index) => (
+                    <ListGroup.Item key={index}>
+                      <Row>
+                        <Col md={3} sm={3} xs={5}>
+                          <Image
+                            src={item.image}
+                            alt={item.name}
+                            fluid
+                            rounded
+                          ></Image>
+                        </Col>
+                        <Col sm={5}>
+                          <Link to={`/productIndividual/${item.productId}`}>
+                            {item.name}
+                          </Link>
+                        </Col>
+                        <Col md={4}>
+                          {item.qty} x Rs.{item.price} = Rs.
+                          {item.qty * item.price}
+                        </Col>
+                      </Row>
+                    </ListGroup.Item>
+                  ))}
+                </ListGroup>
+              )}
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+        <Col md={4} className="px-2">
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <h3 className="placeOrder__heading">ORDER SUMMARY</h3>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Items</Col>
+                  <Col>Rs.{cart.itemsPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Shipping</Col>
+                  <Col>Rs.{cart.shippingPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Tax</Col>
+                  <Col>Rs.{cart.taxPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Total</Col>
+                  <Col>Rs.{cart.totalPrice}</Col>
+                </Row>
+              </ListGroup.Item>
+              {/* <ListGroup.Item>
+                {error && <Message variant="danger">{error}</Message>}
+              </ListGroup.Item> */}
+              <ListGroup.Item>
+                <Button
+                  type="button"
+                  className="btn-block"
+                  disabled={cart.cartItems === 0}
+                  onClick={placeOrderHandler}
+                >
+                  Place Order
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
   );
 }
